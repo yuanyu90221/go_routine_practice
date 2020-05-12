920106a76de252c22f3692268b068ea5c831b4f4
jsonLiang <yuanyu90221@gmail.com>
2020-05-12 23:37:16 +0800

    break loop when errChan got mesage

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -10,16 +11,21 @@ import (
 
 func main() {
 	outChan := make(chan string, 100)
+	errChan := make(chan error, 100)
 	finishChan := make(chan struct{})
 
 	wg := sync.WaitGroup{}
 	wg.Add(20)
 	for i := 0; i < 20; i++ {
-		go func(val int, wg *sync.WaitGroup, out chan<- string) {
+		go func(val int, wg *sync.WaitGroup, out chan<- string, err chan<- error) {
 			time.Sleep(time.Duration(rand.Int31n(1000)) * time.Millisecond)
 			out <- fmt.Sprintf("finished job id: %d", val)
+
+			if val == 15 {
+				err <- errors.New("fail  job in 15")
+			}
 			wg.Done()
-		}(i, &wg, outChan)
+		}(i, &wg, outChan, errChan)
 	}
 	go func() {
 		wg.Wait()
@@ -30,6 +36,9 @@ Loop:
 		select {
 		case out := <-outChan:
 			log.Println(out)
+		case err := <-errChan:
+			log.Println(err)
+			break Loop
 		case <-finishChan:
 			break Loop // break when finish channel receive message
 		}
